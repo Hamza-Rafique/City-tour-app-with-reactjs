85403f570d7a8723d2cb1857278f08a8989ba861
Hamza <hamza.rafique@mobilelive.ca>
2021-03-19 18:13:54 +0500

    city tour app with reactjs

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+import '../../components/logo'
+
+export default function Navbar(){
+return(
+   <nav className='navbar'>
+       <img src=''   />
+
+   </nav>
+);
+
+}
